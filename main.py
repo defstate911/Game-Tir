7c69c7fd66fb2a6be77e347dbb3bf6b46503f202
defstate911 <9114404@gmail.com>
2024-09-07 20:11:01 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
+
+pygame.display.set_caption('Игра ТИР')
+
+icon = pygame.image.load
+
+
 running = True
 while running:
     pass
